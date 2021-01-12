58d65edd00188d82a26051d25fb3c1006d0fa3f4
Lija-Jose <lija.m.j@gmail.com>
2021-01-12 16:15:24 -0600

    Resolved error- 'React' must be in scope when using JSX
    Resolved error- 'React' must be in scope when using JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './Logo/Logo.png';
+import logo from './Images/Logo.png';
 import './App.css';
 import Aux from './hoc/Auxiliary';
 import Auth from './Containers/Auth/Auth';
 import Register from './Containers/Register/Register';
 import {Route, Switch} from 'react-router-dom';
-function App() {
-  
-  
-  return (
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
     <Aux className={"Centralize"}> 
     <img src={logo} className="App-logo" alt="Logo"/>   
     <Switch>
@@ -16,7 +18,8 @@ function App() {
     <Route path="/ForgotPassword" component={Auth}/>
     </Switch>
     </Aux>
-  );
+ );
+}
 }
 
 export default App;
